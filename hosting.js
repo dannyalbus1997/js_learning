545c920e06a1ed8804c523cf7aa69d91b1547fec
talhawajdan <134398365+talhawajdandanny@users.noreply.github.com>
2025-12-31 15:12:05 +0500

    #update

--- hosting.js
+++ hosting.js
@@ -4,25 +4,24 @@ function atmFunc(input) {
   }
   let eightpersontOfotal = input * 0.9;
   let reminder = input - eightpersontOfotal;
-    let denominationCount = findthedenomination(eightpersontOfotal);
-    let denominationCountReminder = findthedenomination(reminder);
-    console.log(eightpersontOfotal);
-    for (let denom in denominationCountReminder) {
-      denominationCount[denom] += denominationCountReminder[denom];
-    }
-    
-    let result = {};
-    for (let denom in denominationCount) {
-      if (denominationCount[denom] > 0) {
-        result[denom] = denominationCount[denom];
-      }
-    }
-    return result;
+  let denominationCount = findthedenomination(eightpersontOfotal);
+  let denominationCountReminder = findthedenomination(reminder);
+  console.log(eightpersontOfotal);
+  for (let denom in denominationCountReminder) {
+    denominationCount[denom] += denominationCountReminder[denom];
+  }
 
+  let result = {};
+  for (let denom in denominationCount) {
+    if (denominationCount[denom] > 0) {
+      result[denom] = denominationCount[denom];
+    }
+  }
+  return result;
 }
 function findthedenomination(input) {
-  const denominations = [5000, 1000, 500,100];
-   let result = {};
+  const denominations = [5000, 1000, 500, 100];
+  let result = {};
   for (let denom of denominations) {
     let count = Math.floor(input / denom);
     input = input % denom;
