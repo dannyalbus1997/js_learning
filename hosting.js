b983604f9d5988287613d148390984b5b8f5e513
talhawajdan <134398365+talhawajdandanny@users.noreply.github.com>
2025-12-31 14:19:14 +0500

    #update

--- hosting.js
+++ hosting.js
@@ -1,4 +1,16 @@
-if (true) {
-  var y = 5;
+function atmFunc(input) {
+    if (input < 500) {
+        return "amount must be at least 500";
+    }
+    const denominations = [ 5000, 1000, 500, 100, 50,10];
+    let result = {};
+    for (let denom of denominations) {
+        let count = Math.floor(input / denom);
+        input = input % denom;
+        result[denom] = count;
+    }
+    return result;
+
+    
 }
-console.log(y);
+console.log(atmFunc(12500));
