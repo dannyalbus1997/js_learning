91a7495f45c21647164bf5feb0469d317eeed78e
talhawajdan <134398365+talhawajdandanny@users.noreply.github.com>
2025-12-31 15:08:46 +0500

    #update

--- hosting.js
+++ hosting.js
@@ -1,16 +1,34 @@
 function atmFunc(input) {
-    if (input < 500) {
-        return "amount must be at least 500";
+  if (input < 500) {
+    return "amount must be at least 500";
+  }
+  let eightpersontOfotal = input * 0.9;
+  let reminder = input - eightpersontOfotal;
+    let denominationCount = findthedenomination(eightpersontOfotal);
+    let denominationCountReminder = findthedenomination(reminder);
+    console.log(eightpersontOfotal);
+    for (let denom in denominationCountReminder) {
+      denominationCount[denom] += denominationCountReminder[denom];
     }
-    const denominations = [ 5000, 1000, 500, 100, 50,10];
+    
     let result = {};
-    for (let denom of denominations) {
-        let count = Math.floor(input / denom);
-        input = input % denom;
-        result[denom] = count;
+    for (let denom in denominationCount) {
+      if (denominationCount[denom] > 0) {
+        result[denom] = denominationCount[denom];
+      }
     }
     return result;
 
-    
 }
-console.log(atmFunc(12500));
+function findthedenomination(input) {
+  const denominations = [5000, 1000, 500,100];
+   let result = {};
+  for (let denom of denominations) {
+    let count = Math.floor(input / denom);
+    input = input % denom;
+    result[denom] = count;
+  }
+  return result;
+}
+
+console.log(atmFunc(25000));
